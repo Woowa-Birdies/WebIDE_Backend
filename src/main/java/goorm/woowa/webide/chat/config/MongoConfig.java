d2cf44a2eb29780d1d13479ebdf28c4daf0efb33
Jhsysng <ysng1118@naver.com>
2024-02-12 16:35:26 +0900

    feat: mongo config, document 추가

---
+++ src/main/java/goorm/woowa/webide/chat/config/MongoConfig.java
@@ -0,0 +1,25 @@
+package goorm.woowa.webide.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
+                                                       MongoMappingContext mongoMappingContext)  {
+
+        DefaultDbRefResolver defaultDbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter mappingMongoConverter = new MappingMongoConverter(defaultDbRefResolver, mongoMappingContext);
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return mappingMongoConverter;
+
+    }
+}
