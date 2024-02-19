d00f5dbb16f2fbdde590d14ef37b58ca584cced9
Jhsysng <ysng1118@naver.com>
2024-02-19 17:51:10 +0900

    feat: jwt interceptor

--- src/main/java/goorm/woowa/webide/config/WebSocketConfig.java
+++ src/main/java/goorm/woowa/webide/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package goorm.woowa.webide.config;
 
+import goorm.woowa.webide.common.handler.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,14 +13,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    //todo jwt intercepter
+    private final JwtInterceptor jwtInterceptor;
     //todo error handler
     // custom shake handler
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         // /sub에게 모두 전달
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/queue");
 
         // /pub 시작 메시지가 message-handling methods로 라우팅
         registry.setApplicationDestinationPrefixes("/pub");
@@ -28,8 +30,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .addInterceptors()
                 .setAllowedOrigins("*");
         //todo: errorhandler, interceptor, handshake handler 추가
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtInterceptor);
     }
 
     //todo : 웹소켓 커넥션, 데이터 크기 설정 컨테이너 관리 bean 생성
