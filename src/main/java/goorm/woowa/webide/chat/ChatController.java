79bec71194ea2d0bb07217fbae873b28c2a62951
Han <inte098@naver.com>
2024-02-25 19:05:59 +0900

    test: 출력 테스트

--- src/main/java/goorm/woowa/webide/chat/ChatController.java
+++ src/main/java/goorm/woowa/webide/chat/ChatController.java
@@ -9,8 +9,7 @@ public class ChatController {
     @MessageMapping("/chat")    // 클라이언트 -> 서버 "/pub/chat"
     @SendTo("/sub/chat")        // 구독자에게 전달
     public transmitMessage chat(receiveMessage message) throws Exception {
-        Thread.sleep(5000);
-        return new transmitMessage("서버에서 전송 : " + message.getMessage());
+        return new transmitMessage(message.getMessage());
         // transmitMessage, receiveMessage : 테스트용
     }
 }
